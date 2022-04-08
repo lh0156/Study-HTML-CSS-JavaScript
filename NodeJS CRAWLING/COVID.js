2120a35c612f579c07ca8da6ce3d517cdee119ec
YunSeop <62834176+lh0156@users.noreply.github.com>
2022-04-08 09:32:31 +0900

    add NODE CRAWLING

--- NodeJS CRAWLING/COVID.js
+++ NodeJS CRAWLING/COVID.js
@@ -3,7 +3,7 @@ const cheerio = require("cheerio");
 
 const getHTML = async(keyword) => {
     try {
-        return await axios.get("https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=0&ie=utf8&query=%EC%BD%94%EB%A1%9C%EB%82%98+19+%ED%99%95%EC%A7%84%EC%9E%90")
+        return await axios.get("https://search.naver.com/search.naver?sm=tab_hty.top&where=nexearch&query=%EB%B0%A9%EC%97%AD%EC%A7%80%EC%B9%A8&oquery=%EC%BD%94%EB%A1%9C%EB%82%98+19+%ED%99%95%EC%A7%84%EC%9E%90&tqi=hC8MVdp0JywsslA%2F6FKssssst%2Fs-070936")
     } catch(err) {
         console.log(err);
     }
@@ -12,32 +12,22 @@ const getHTML = async(keyword) => {
 const parsing = async (keyword) => {
     const html = await getHTML(keyword);
     const $ = cheerio.load(html.data);
-    const $status_info = $(".status_info");
+    const $status_info = $(".info_video");
+    
+    const guideList = [];
 
-    const confirmedList = [];
-    const severeList = [];
-    const new_hospitalizationList = [];
-    const dieList = [];
+    $status_info.each(async (index,node) => {
 
-    $status_info.each((index,node) => {
-
-        const confirmed = $(node).find(".info_01 .info_num").text();
-        const severe = $(node).find(".info_02 .info_num").text();
-        const new_hospitalization = $(node).find(".info_03 .info_num").text();
-        const die = $(node).find(".info_04 .info_num").text();
+        const guide = $(node).find(".scm_ellipsis a").text();
         
-        confirmedList.push(confirmed);
-        severeList.push(severe);
-        new_hospitalizationList.push(new_hospitalization);
-        dieList.push(die);
+        guideList.push(guide);
+
+
     })
 
-    console.log(`국내 확진자: ${confirmedList[0]}`);
-    console.log(`국내 재원 위중증: ${severeList[0]}`);
-    console.log(`국내 신규입원: ${new_hospitalizationList[0]}`);
-    console.log(`국내 사망자: ${dieList[0]}`);
+    console.log(guideList[0]);
 
 }
 
-
-parsing();
+    parsing();
+    
