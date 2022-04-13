64c93875229f31cb2cce30692e6a6bebb468c6c2
YunSeop <62834176+lh0156@users.noreply.github.com>
2022-04-13 18:39:39 +0900

    add Node JS Crawling

--- NodeJS CRAWLING/COVID.js
+++ NodeJS CRAWLING/COVID.js
@@ -3,7 +3,7 @@ const cheerio = require("cheerio");
 
 const getHTML = async(keyword) => {
     try {
-        return await axios.get("https://search.naver.com/search.naver?sm=tab_hty.top&where=nexearch&query=%EB%B0%A9%EC%97%AD%EC%A7%80%EC%B9%A8&oquery=%EC%BD%94%EB%A1%9C%EB%82%98+19+%ED%99%95%EC%A7%84%EC%9E%90&tqi=hC8MVdp0JywsslA%2F6FKssssst%2Fs-070936")
+    return await axios.get("https://search.naver.com/search.naver?sm=tab_hty.top&where=nexearch&query=%EB%B0%A9%EC%97%AD%EC%A7%80%EC%B9%A8&oquery=%EC%BD%94%EB%A1%9C%EB%82%98+19+%ED%99%95%EC%A7%84%EC%9E%90&tqi=hC8MVdp0JywsslA%2F6FKssssst%2Fs-070936")
     } catch(err) {
         console.log(err);
     }
@@ -21,16 +21,17 @@ const parsing = async (keyword) => {
         const guide = $(node).find(".scm_ellipsis a").text();
         
         guideList.push(guide);
+
+        return guide;
         
         
     })
     
-    return guideList[0];
+    console.log(guideList[0]);
     
 }
 
 
     const ment = parsing();
-    console.log(ment);
-
+    
     
