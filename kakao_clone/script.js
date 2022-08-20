be6c92fc291bd6a71cc4fa7055f489aa506653f3
Eom Yunseop <lh0156@naver.com>
2022-08-20 21:49:52 +0900

    change file form

---
+++ kakao_clone/script.js
@@ -0,0 +1,14 @@
+setTimeout(getTime, 100);
+
+function getTime() {
+    let d = new Date();	// 현재 날짜와 시간
+    let hur = d.getHours();		// 시
+    let min = d.getMinutes();	//분
+
+    let timeBoard = document.querySelector("#status-bar_Time");
+
+    let time = hur + ":" + min;
+
+    timeBoard.innerHTML = time;
+
+}
